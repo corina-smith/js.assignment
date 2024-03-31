1e7cf07da229462a7d979d31086a5697484b7f15
corina-smith <165103502+corina-smith@users.noreply.github.com>
2024-03-30 18:51:17 -0700

    Add files via upload

---
+++ Activity-JS-Function-StartFiles/js-demo.js
@@ -0,0 +1,30 @@
+﻿/*
+   JS Activity Clock
+   Corina Smith
+   3/28/2024    
+
+   function getWeekday(dayNum)
+      Returns the text of the day of the week where dayNum
+      is the number of the week from 0 (Sunday) to 6 (Saturday)
+*/
+
+runClock();
+setInterval("runClock()", 1000);
+
+function runClock(){
+   var thisDay = new Date();
+   var thisDate = thisDay.toLocaleDateString();
+   var thisDayNum = thisDay.getDay();
+   var thisWeekday = getWeekday(thisDayNum);
+   var thisTime = thisDay.toLocaleTimeString();
+
+   //the following code it to attach to the HTML//
+   document.getElementById("date").textContent = thisDate;
+   document.getElementById("wday").textContent = thisWeekday;
+   document.getElementById("time").textContent = thisTime;
+}
+
+function getWeekday(dayNum) {
+   var wDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+   return wDays[dayNum];
+}
